81f31279e2c175f3c71fbb5768e8021d74ede528
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-10-14 08:49:32 -0400

    read JSON file from raw folder

--- app/src/main/java/ca/georgiancollege/comp3025_f2022_week6/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/comp3025_f2022_week6/MainActivity.kt
@@ -1,11 +1,24 @@
 package ca.georgiancollege.comp3025_f2022_week6
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+       val contactListString = getTextFromResource(this, R.raw.contacts)
+
+        Log.i("contact_list", contactListString)
+    }
+
+    fun getTextFromResource(context: Context, resourceId: Int): String
+    {
+        return context.resources.openRawResource(resourceId)
+            .bufferedReader()
+            .use {it.readText()}
     }
 }
